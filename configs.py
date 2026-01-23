98e0339ba30e8c7b41d46a047f9ff781517c708d
fmusio <fabio.musio@protonmail.com>
2026-01-23 18:18:28 +0100

    add flag dynamic angle computation

--- configs.py
+++ configs.py
@@ -17,7 +17,7 @@ nnunet_results = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'predi
 os.environ["nnUNet_results"] = nnunet_results
 
 ################## Modality ##################
-modality = 'mr' # 'ct' or 'mr'
+modality = 'ct' # 'ct' or 'mr'
 
 ################## Main Pipeline ##################
 # Bool args for main pipeline
@@ -31,7 +31,7 @@ compute_radius_along_graph = True
 extract_features = True
 
 ### Args Step 1: Cropping, mask correction and resampling
-do_mask_corrections = True # perform mask corrections or not
+do_mask_corrections = False # perform mask corrections or not
 # NOTE: For skipping individual mask correction steps, set variables below to None
 min_overall_segment_size = 30 # minimum overall segment size (#voxels) to keep after cropping (before resampling)
 threshold_for_component_removal = 15 # threshold for removing small disconnected components (#voxels) (before resampling)
@@ -50,8 +50,9 @@ median_a1=15.6 # if acom absent, define a1 as segment with constant length
 median_c7=7.1 # if pcom absent, define c7 as segment with constant length
 margin_from_cow=10 # marginal length from cow for BA/P2/C6/MCA/A2/3rd-A2 segments
 ## bifurcation features
-dist_angle = 1 # distance [mm] to sample points around bifurcation for angle computation
-nr_pts_angle_average = 1 # number of points to average for angle computation
+fixed_dist_angle = False # whether to use fixed distance for angle computation or dynamic distance to segment boundary points (only for major bifurcations BA/ICA)
+dist_angle = 1.5 # distance [mm] to sample points around bifurcation for angle computation
+nr_pts_angle_average = 3 # number of points to average for angle computation
 nr_edges_rad_avg = 3 # number of edges to average for radius computation
 fixed_dist_radius = False # whether to use fixed distance for radius computation or dynamic distance (max dist from bifurcation to boundary points)
 ## fetal pca
